52f1adfb84573135c2187b665774e23e4890d978
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2023-01-18 22:21:55 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -45,7 +45,7 @@ data "aws_subnet" "subnet" {
 ########### Provider ###########
 
 provider "aws" {
-    region = var.region
+    region     = var.region
     access_key = var.access_key
     secret_key = var.secret_key
 }
